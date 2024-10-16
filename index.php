f888a97c8dae0a0e0c4d007378cea799ac05a8a8
workshop-ims <futuroluisambrosio@gmail.com>
2024-10-16 16:59:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="enviar_email.php" method="post">
+    Nome: <input type="text" name="nome" required><br>
+    E-mail: <input type="email" name="email" required><br>
+    Mensagem: <textarea name="mensagem" required></textarea><br>
+    <input type="submit" value="Enviar">
+</form>
+s
