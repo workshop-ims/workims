933cbf1948f156c4445142ec3999af461fa9f6d6
workshop-ims <futuroluisambrosio@gmail.com>
2024-10-19 16:18:03 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<form action="enviar_email.php" method="post">
-    Nome: <input type="text" name="nome" required><br>
-    E-mail: <input type="email" name="email" required><br>
-    Mensagem: <textarea name="mensagem" required></textarea><br>
-    <input type="submit" value="Enviar">
-</form>
-s
